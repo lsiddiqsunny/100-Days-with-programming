c5f3e8c213656461d0a453a516ff7b9e1ee58493
Latif siddiq sunny <lsiddiqsunny@gmail.com>
2020-07-20 22:28:38 +0600

    updated

---
+++ Contest/July Circuits '20/1.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    int test;
+    cin>>test;
+    while(test--){
+        long long n;
+        cin>>n;
+        cout<<1LL+(n*(n+1))/2<<endl;
+    }
+}
